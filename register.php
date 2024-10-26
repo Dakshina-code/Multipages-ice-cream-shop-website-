34ce1f63270d5496eb5d446090cf3653bcae8df9
Tharindu Dakshina <tharindudakshina2004@gmail.com>
2024-10-26 23:39:32 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -1,6 +1,5 @@
 <?php
-
-include '../components/connect.php'
+include '../components/connect.php';
 
       if (isset($_POST['submit'])) {
     
@@ -20,8 +19,27 @@ include '../components/connect.php'
 
                     $image = $_FILES['image']['name'];
                     $image = filter_var($image, FILTER_SANITIZE_STRING);
-                    $ext = pathinfo(path)
+                    $ext = pathinfo($image, PATHINFO_EXTENSION);
+                    $rename = unique_id().'.'.$ext;
+                    $image_size = $_FILES['image']['size'];
+                    $image_tmp_name = $_FILES['image']['tmp_name'];
+                    $image_folder = '../uploaded_files/'.$rename;
+                    
+                    $select_seller = $conn->prepare("SELECT * FROM `sellers` WHERE email=?");
+                    $select_seller->execute([$email]);
 
+                    if ($select_seller->rowCount() > 0 ) {
+                        $warning_msg[] = 'email already exist';
+                    }else {
+                        if ($pass != $cpass) {
+                             $warning_msg[] = 'confirm password not matched';
+                        }else {
+                            $insert_seller = $conn->prepare("INSERT INTO `sellers`(id, name, email, password, image) VALUES(? , ?, ?, ?, ?)");
+                            $insert_seller->execute([$id, $name, $email, $cpass, $rename]);
+                            move_uploaded_file($image_tmp_name, $image_folder);
+                            $success_msg[] = 'new seller registered! please loging now';
+                        }
+                    }
     }
 
 
@@ -32,7 +50,7 @@ include '../components/connect.php'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Blue Sky Summer - seller registration page</title>
     <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
 
     <!-- font awesome cdn link  -->
